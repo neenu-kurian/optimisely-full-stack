bf3a60277c37a7055030f540749b9d2fc09c1998
Neenu Kurian <nkurian@salmon.com>
2019-06-18 09:09:23 +0200

    webhook

--- server/src/app.js
+++ server/src/app.js
@@ -24,10 +24,10 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-const TOKEN = "2bOOX7v97oBMgm0KBgR_pJmWH-fM9dhl-nD84N5w3lk";
+const TOKEN = "PPwWaWGC8rrBx6dJhPyKrJ0_1bq_0gn1-tYnoWJ_OC0";
 let encrypted = encrypt(TOKEN);
 console.log("encrypted one", encrypted);
-let decrypted= decrypt(encrypted);
+let decrypted = decrypt(encrypted);
 console.log("decrpt", decrypted);
 
 app.use(function(req, res, next) {
@@ -129,7 +129,6 @@ router.post("/login", function(req, res) {
       }
 
       if (username.includes("user1")) {
-        
         optimizelyClientInstance.setForcedVariation(
           "sorting_experiment",
           "user1",
@@ -142,7 +141,6 @@ router.post("/login", function(req, res) {
         //   attributes
         // );
 
-
         console.log("user is bucketed into variation1");
         res.send([
           {
@@ -154,23 +152,22 @@ router.post("/login", function(req, res) {
           }
         ]);
       } else if (username.includes("user2")) {
-           optimizelyClientInstance.setForcedVariation(
-             "sorting_experiment",
-             "user2",
-             "Menu_2"
-           );
+        optimizelyClientInstance.setForcedVariation(
+          "sorting_experiment",
+          "user2",
+          "Menu_2"
+        );
 
         // var variation2 = optimizelyClientInstance.activate(
         //   "sorting_experiment",
         //   username,
         //   attributes
         // );
-     
 
         console.log("user is bucketed into variation2");
         res.send([
           {
-            variation: 'Menu_2',
+            variation: "Menu_2",
             enabled: enabled,
             appetizer: appt,
             dessert: dessert,
@@ -209,24 +206,29 @@ router.post("/dessert", function(req, res) {
 
 router.post("/", function(req, res) {
   console.log("body", req.body);
+
+  var request_signature = req.headers["x-hub-signature"];
+  var computed_signature = "sha1=" + TOKEN;
+
+  var mismatch = 0;
+  for (var i = 0; i < request_signature.length; ++i) {
+    mismatch |=
+      request_signature.charCodeAt(i) ^ computed_signature.charCodeAt(i);
+  }
   
-   rp(options).then(function(datafile) {
-  optimizelyClientInstance = optimizelySDK.createInstance({
-    datafile: datafile,
-    logger: defaultLogger.createLogger({
-      logLevel: LOG_LEVEL.INFO
-    }),
-    errorHandler: defaultErrorHandler
-  });
-});
+  console.log('mismatch',mismatch);
 
-   var request_signature= req.headers;
-   var computed_signature='sha1='+TOKEN;
-
-  console.log("request header", req.headers);
-
-  // var request_signature = JSON.stringify(req.headers).x - hub - signature;
-  // var computed_signature="sha1="+ TOKEN;
+  if (!mismatch) {
+    rp(options).then(function(datafile) {
+      optimizelyClientInstance = optimizelySDK.createInstance({
+        datafile: datafile,
+        logger: defaultLogger.createLogger({
+          logLevel: LOG_LEVEL.INFO
+        }),
+        errorHandler: defaultErrorHandler
+      });
+    });
+  }
 });
 
 function encrypt(text) {
